58b2ac3afd38c9eb4caa7fe7c1824dff718c459a
Vipul Gautam <wtech905@gmail.com>
2019-11-21 22:29:39 +0530

    auth angular compnent created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,25 +11,30 @@ import {
   MatProgressSpinnerModule,
   MatPaginatorModule
 } from '@angular/material';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { HeaderComponent } from './header/header.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PostCreateComponent,
     HeaderComponent,
-    PostListComponent
+    PostListComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
+    FormsModule,
     MatInputModule,
     MatProgressSpinnerModule,
     MatCardModule,
