4ac87623a4984f7a0b96370f04386c500af9c265
Vipul Gautam <wtech905@gmail.com>
2020-01-10 23:04:46 +0530

    controller added

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,132 +1,17 @@
 const express = require('express');
-const Post = require('../models/post');
-const multer = require('multer');
 const checkJWT = require('../middleware/check-auth');
+const extractFile = require('../middleware/file');
 const router = express.Router();
+const postController = require('../controllers/posts');
 
-const MIME_TYPE_MAP = {
-  'image/png': 'png',
-  'image/jpeg': 'jpg',
-  'image/jpg': 'jpg'
-};
+router.get('/', postController.getPosts);
 
-router.get('/', (req, res, next) => {
-  const pageSize = +req.query.pageSize; // + will convert string to number
-  const curentPage = +req.query.page;
-  const postQuery = Post.find();
-  let fetchedPosts;
-  if (pageSize && curentPage) {
-    postQuery
-      .skip(pageSize * (curentPage - 1))
-      .limit(pageSize);
-  }
-  postQuery.then((document)=>{
-    fetchedPosts = document;
-    return Post.countDocuments();
-  })
-  .then(count => {
-    res.status(200).json({
-      message: 'Post fetched successfully',
-      posts: fetchedPosts,
-      maxPosts: count
-    });
-  })
-  .catch(error => { res.status(500).json({
-    message: 'Fetching posts failed!'
-  }); });
-});
+router.get('/:id', postController.getPost);
 
-router.get('/:id', (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if(post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: 'Post not found' });
-    }
-  })
-  .catch(error => res.status(500).json({
-    message: 'Fetching Post failed!'
-  }));
-});
+router.post('', checkJWT, extractFile, postController.createPost);
 
+router.put('/:id',checkJWT, extractFile, postController.updatePost);
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error('Invalid mime type');
-    if (isValid) {
-      error = null;
-    }
-    cb(error, 'backend/images');
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(' ').join('_');
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + '_' + Date.now() + '.' + ext);
-  }
-});
-
-router.post('', checkJWT, multer({storage}).single('image'), (req, res, next) => {
-  const url = req.protocol + '://' + req.get('host');
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: url + '/images/' + req.file.filename,
-    creator: req.userData.userId
-  });
-  post.save().then((createdPost) => {
-    res.status(201).json({
-      message: 'Post addeded successfully!',
-      post: {
-        ...createdPost,
-        id: createdPost._id,
-      }
-    })
-  })
-  .catch(error => {
-    res.status(500).json({
-      message: 'Creating a post failed!'
-    });
-  });
-});
-
-router.put('/:id',checkJWT, multer({storage}).single('image'), (req, res, next) => {
-  let imagePath = req.body.imagePath; // imagePath is string
-  if (req.file) {
-    const url = req.protocol + '://' + req.get('host');
-    imagePath = url + "/images/" + req.file.filename;
-  }
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content,
-    imagePath: imagePath,
-    creator: req.userData.userId
-  });
-  Post.updateOne({_id: req.params.id, creator: req.userData.userId }, post).then(updatedPost => {
-    if (updatedPost.nModified > 0) {
-      res.status(200).json({ message: 'Updated Successfully!' });
-    } else {
-      res.status(401).json({ message: 'Not Authorization' });
-    }
-
-  }).catch(error => { res.status(500).json({
-    message: 'Couldn\'t update post!'
-  }); })
-});
-
-router.delete('/:id', checkJWT, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then((result) => {
-    if (updatedPost.n > 0) {
-      res.status(200).json({ message: 'post deleted successfully!' });
-    } else {
-      res.status(401).json({ message: 'Not Authorization' });
-    }
-  })
-  .catch((reason) => {
-    res.status(500).json({ message: 'Something went wrong!' });
-    console.log(reason);
-  });
-});
+router.delete('/:id', checkJWT, postController.deletePost);
 
 module.exports = router;
